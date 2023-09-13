9c4aaa9b688329b9594cf9a30310fd341698baef
Dockal <jamesdockal@gmail.com>
2023-09-13 14:23:02 -0300

    feat: register menu diurno and noturno on startup with not registered

--- src/services/prisma.service.ts
+++ src/services/prisma.service.ts
@@ -5,5 +5,20 @@ import { PrismaClient } from '@prisma/client';
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
+
+    await this.menu.upsert({
+      where: { name: 'Diurno' },
+      update: {},
+      create: {
+        name: 'Diurno',
+      },
+    });
+    await this.menu.upsert({
+      where: { name: 'Noturno' },
+      update: {},
+      create: {
+        name: 'Noturno',
+      },
+    });
   }
 }
